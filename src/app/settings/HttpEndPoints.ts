074b2a66a2502e9a80f382469d7f27c77d5ab2e4
Mahmoud Khaled <Mhmoud-Khaled>
2024-05-03 15:27:58 +0300

    add needed endpoints paths

---
+++ src/app/settings/HttpEndPoints.ts
@@ -0,0 +1,5 @@
+export class HttpEndPoints {
+  GetAllCattegories = 'https://fakestoreapi.com/products/categories'
+  GetInCategory = 'https://fakestoreapi.com/products/category/{sub-category}'
+  GetProductDetails = 'https://fakestoreapi.com/products/{id}'
+}
