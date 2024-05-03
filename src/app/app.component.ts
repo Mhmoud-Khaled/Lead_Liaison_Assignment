3a7dc3e918b185eb9925137e30ba249fbbc0af52
Mahmoud Khaled <Mhmoud-Khaled>
2024-05-04 00:09:51 +0300

    check the connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
-export class AppComponent {
-    title = 'LeadLiaison_Assignment';
+export class AppComponent implements OnInit {
+  title = 'LeadLiaison_Assignment';
+
+  constructor(
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit() {
+    this.checkTheInternet()
+  }
+
+  checkTheInternet() {
+    window.addEventListener('offline', () => this.toastr.warning("You are offline, please check the internet"));
+    window.addEventListener('online', () => this.toastr.success("the internet is restored"));
+  }
 }
