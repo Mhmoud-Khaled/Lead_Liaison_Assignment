0a5f0a8f3f576b7739aa84ac2a48d545de341dd6
Mahmoud Khaled <Mhmoud-Khaled>
2024-05-03 21:43:04 +0300

    design UI for product details implement product details - get id of product from url - fetch product details by calling API - convert rate number to array for dispalying number of star according to rate

--- src/app/home/card-details/card-details.component.ts
+++ src/app/home/card-details/card-details.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { iProduct } from '../../settings/dataModel';
+import { ActivatedRoute } from '@angular/router';
+import { HttpEndPoints } from '../../settings/HttpEndPoints';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-card-details',
   templateUrl: './card-details.component.html',
   styleUrl: './card-details.component.scss'
 })
-export class CardDetailsComponent {
+export class CardDetailsComponent implements OnInit {
+
+  rate: any[] = []
+  product!: iProduct
+  showSpinner: boolean = true
+
+
+  constructor(
+    private ActivatedRoute: ActivatedRoute,
+    private HttpClient: HttpClient
+  ) { }
+
+  ngOnInit() {
+    this.GetIdOfProduct()
+  }
+
+  GetIdOfProduct() {
+    this.ActivatedRoute.params.subscribe(params => {
+      const id = params['id'];
+      this.GetProductdetails(id)
+    });
+  }
+
+  GetProductdetails(id: number) {
+    this.showSpinner = true
+    this.rate = []
+    let httpEndPoints = HttpEndPoints.store.GetProductDetails
+    httpEndPoints = httpEndPoints.replace('{id}', String(id))
+    this.HttpClient.get(httpEndPoints).subscribe(
+      (response: any) => {
+        this.product = response
+        this.product.rating.rate = Math.round(this.product.rating.rate)
+        let range = Array.from({ length: this.product.rating.rate }, (_, i) => i + 1);
+        this.rate = range
+        this.showSpinner = false
+      },
+      (error: any) => {
+        console.log(error)
+      }
+    )
+  }
 
 
 }
